200ecf78546fda4032c4b333a6c440bc909a1a06
Yudaotor <yudaotor@qq.com>
2023-02-26 12:35:11 +0800

    add try catch

--- src/VersionManager.py
+++ src/VersionManager.py
@@ -4,12 +4,15 @@ class VersionManager:
 
     @staticmethod
     def getLatestTag():
-        latestTagResponse = req.get("https://github.com/Yudaotor/CapsuleFarmerEvolved-dingding/releases/latest")
-        if 'application/json' in latestTagResponse.headers.get('Content-Type', ''):
-            latestTagJson = latestTagResponse.json()
-            if "tag_name" in latestTagJson:
-                return float(latestTagJson["tag_name"][1:])
-        return 0.0
+        try:
+            latestTagResponse = req.get("https://github.com/Yudaotor/CapsuleFarmerEvolved-dingding/releases/latest")
+            if 'application/json' in latestTagResponse.headers.get('Content-Type', ''):
+                latestTagJson = latestTagResponse.json()
+                if "tag_name" in latestTagJson:
+                    return float(latestTagJson["tag_name"][1:])
+            return 0.0
+        except Exception:
+            print("查找是否版本是否为最新时发生错误")
     @staticmethod
     def isLatestVersion(currentVersion):
         return currentVersion >= VersionManager.getLatestTag()
--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ from SharedData import SharedData
 from Stats import Stats
 from VersionManager import VersionManager
 
-CURRENT_VERSION = 1.42
+CURRENT_VERSION = 1.43
 
 def init() -> tuple[logging.Logger, Config]:
     parser = argparse.ArgumentParser(description='Farm Esports Capsules by watching all matches on lolesports.com.')
